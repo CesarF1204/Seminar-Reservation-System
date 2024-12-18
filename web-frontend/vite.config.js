bec93eb59c4754f20d9fd334fb1eca4d121aa68c
Cesar Francisco <princexcesar@gmail.com>
2024-12-18 17:49:54 +0800

    fixing styles not showing on prod

--- web-frontend/vite.config.js
+++ web-frontend/vite.config.js
@@ -3,15 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': import.meta.env.VITE_API_BASE_URL, // Backend URL
-    },
-    port: process.env.PORT || 5173
-  },
   // base: './',
   plugins: [react()],
-  // build: {
-  //   cssCodeSplit: true,
-  // },
 })
