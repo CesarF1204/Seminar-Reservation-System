52696ba4e4e9dc19aa560dd15b8d55049d3f679a
Cesar Francisco <princexcesar@gmail.com>
2024-12-19 10:31:24 +0800

    fixing issue: cant login the deployed site due to blocking of cookie

--- web-backend/controllers/authController.js
+++ web-backend/controllers/authController.js
@@ -98,7 +98,7 @@ const login = async (req, res) => {
             httpOnly: true, /* Prevent JavaScript from accessing the cookie */
             secure: false, /* Use secure flag in production */
             maxAge: 86400000, /* Cookie expires in 1 day */
-            sameSite: "none",
+            sameSite: process.env.NODE_ENV === "production" ? "Strict" : "None",
         });
 
         /* Send a success response with the token and user's details */
--- web-backend/routes/authRoutes.js
+++ web-backend/routes/authRoutes.js
@@ -47,8 +47,8 @@ router.get('/validate-token', verifyToken, (req, res) => {
 router.post("/logout", (req, res) => {
         res.cookie("auth_token", "", {
         expires: new Date(0),
-        secure: false,
-        sameSite: "none",
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "Strict" : "None",
     });
     res.send();
 });
